63690cd327caf591acf3cecdf1b478de8366f8dd
ArnoldListvan <101814654+ArnoldListvan@users.noreply.github.com>
2022-03-22 08:25:48 +0200

    Add files via upload

---
+++ bite/src/test/java/com/taskProject/bite/BiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskProject.bite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
